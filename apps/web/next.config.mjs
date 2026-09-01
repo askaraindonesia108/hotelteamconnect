5c2a133c9f697ebfae62eee04d85dc32d04e9909
Villa Omahku <askaraindonesia201@gmail.com>
2026-09-02 03:08:56 +0700

    Fix webpack error for argon2 native module

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Memaksa Vercel untuk tetap deploy walaupun ada error tipe data
     ignoreBuildErrors: true,
   },
   experimental: {
-    serverComponentsExternalPackages: ['node-zklib'],
+    serverComponentsExternalPackages: ['node-zklib', '@node-rs/argon2'],
   },
 };
 
